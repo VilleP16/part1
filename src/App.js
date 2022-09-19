6f9bff55c0a14925522e4824390c5352d15ccf73
villep <pekvil16@gmail.com>
2022-09-19 13:10:00 +0300

    testailuja

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-const App = () => (
+
+const Hello =  (props) =>{
+  return(
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+  return(
   <div>
-    <p>Hello world</p>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={26+10}/>
+    <Hello name={nimi} age={ika}/>
   </div>
-)
+  )
+ }
 
 export default App
